38df4e76f6662bdfa5cf86ed212ea7527a1b5942
IsratMoon <mnisrat.j@gmail.com>
2023-09-06 18:15:50 +0600

    Modification

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -8,7 +8,7 @@ const useAxios = () => {
     const { logOut } = useAuth();
 	const navigate = useNavigate();
 	const axiosSecure = axios.create({
-		baseURL: "http://127.0.0.1:5000",
+		baseURL: "http://localhost:3000/",
 	});
 
 	useEffect(() => {
